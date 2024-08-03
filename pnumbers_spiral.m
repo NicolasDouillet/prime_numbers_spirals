48e7ddf524f88182c77f41af82c02d8e5c6bbfa7
NicolasDouillet <nicolas.douillet@free.fr>
2024-08-03 20:27:07 +0200

    + circles Z color, + radial colormap, + black font

--- pnumbers_spiral.m
+++ pnumbers_spiral.m
@@ -2,7 +2,7 @@ function S = pnumbers_spiral(N, option_display)
 %% pnumbers_spiral : function to display the spiral of prime numbers laying
 % on 6n +/- 1 circles, and check its sum.
 %
-% Author : nicolas.douillet9 (at) gmail.com, 2023-2024.
+% Author : nicolas.douillet (at) free.fr, 2023-2024.
 %
 %
 % Inputs
@@ -15,6 +15,11 @@ function S = pnumbers_spiral(N, option_display)
 % Output
 %
 % - S : integer scalar, equals to 1 + the sum of primes less or equal to N.
+
+
+% TODO :
+%
+% + 2x2 squares  in rectangle triangles
 
 
 %% Input parsing
@@ -72,42 +77,54 @@ isequal(mod(S,6) < length(v)-1,true) % length(v)-1 since 1 not prime doesn't cou
 if option_display
     
     figure(1);
-    set(gcf,'Color',[1 1 1]);
+    set(gcf,'Color',[0 0 0]);
     
-    plot3(P(1,:),P(2,:),P(3,:),'o','Color',[0 0 0],'Linewidth',3), hold on;
+    plot3(P(1,:),P(2,:),P(3,:),'o','Color',[1 1 1],'Linewidth',3), hold on;
     line(P(1,:),P(2,:),P(3,:),'Color',[0.875 0 0],'Linewidth',2), hold on;
     
     % Rectangle triangles and Z axis display option
     for k = 1:size(P,2)
     
-       line([0,P(1,k)],[0,P(2,k)],[P(3,k),P(3,k)],'Color',[0 0 0.875],'Linewidth',2), hold on;
+       line([0,P(1,k)],[0,P(2,k)],[P(3,k),P(3,k)],'Color',[1 1 1],'Linewidth',2), hold on; % [0 0 0.875]
     
     end
-    %
-    % line([0 0],[0 0],[v(1,1) v(1,end)],'Color',[0 0 1],'Linewidth',2), hold on;
+   
+    line([0 0],[0 0],[v(1,1) v(1,end)],'Color',[1 1 1],'Linewidth',2), hold on;
     
     axis square, axis equal;
     grid on;
     view(2);
-    gca.Cipping = 'off';
+    % gca.Cipping = 'off';
     
     
     % Plot polar centered circles of prime radii
     pm_pot_idx = mod(u.^2-1,24) == 0;
     pm_pot_idx(1) = 0;
+    cmap = colormap(jet);    
+    alpha = linspace(0,2*pi,361);
+    base_circle_X = cos(alpha);
+    base_circle_Y = sin(alpha);
     
     for k = u(pm_pot_idx)
         
-        rectangle('Position',[-k -k 2*k 2*k],'Curvature',[1 1]), hold on; % + z = r
+        line(k*base_circle_X,k*base_circle_Y,k*ones(1,numel(alpha)),'Color',cmap(round(k*size(jet,1)/numel(u)),:)), hold on;
         
     end
     
     xlabel('Signed integer unit','FontSize',16)
     ylabel('Signed integer unit','FontSize',16)
     title({['Spiral of prime numbers from 5 to ',num2str(N)],'laying on 6n \pm 1 radii / circles'},'FontSize',16);
-    legend('Position of primes $$p = 6n \pm 1$$ on the circles','Distance $$l = \sqrt{p''^2-p^2}$$ between consecutives primes','Radii / values of primes','Interpreter','latex','FontSize',12);    
+    legend('Position of primes $$p = 6n \pm 1$$ on the circles','Distance $$l = \sqrt{p''^2-p^2}$$ between consecutives primes',...
+           'Radii / values of primes','Interpreter','latex','FontSize',12,'Color',[0 0 0],'EdgeColor',[1 1 1],'TextColor',[1 1 1],'FontSize',12);
+
+%     xlabel('Rayon (unité entière)','FontSize',16);
+%     ylabel('Rayon (unité entière)','FontSize',16);
+%     title({['Spirale des nombres premiers de 5 à ',num2str(N)],'basée sur les cercles de rayons 6n \pm 1'},'FontSize',16); % + vue polaire   
+%     legend('Position des premiers $$p = 6n \pm 1$$ sur les cercles','Distance $$l = \sqrt{p''^2-p^2}$$ entre les premiers consecutifs',...
+%            'Rayons / valeurs des premiers','Interpreter','latex','FontSize',12,'Color',[0 0 0],'EdgeColor',[1 1 1],'TextColor',[1 1 1]);
     
     box on;
+    set(gca,'Color',[0 0 0],'XColor',[1 1 1],'YColor',[1 1 1],'ZColor',[1 1 1]);
     
 end
 
